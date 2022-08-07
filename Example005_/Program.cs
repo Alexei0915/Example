a9874b5f3ea82559828d3255ac9e73a5d38de461
Alexei <vertuginalexei7@gmail.com>
2022-08-07 19:34:32 +0300

    Exampl005

---
+++ Example005_/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "алексей")
+{
+    Console.WriteLine("Ура,это же Алексей");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
