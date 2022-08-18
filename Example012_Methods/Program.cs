e2197a35459a38be99a5fc2ed6a26f0207937de7
Alexei <vertuginalexei7@gmail.com>
2022-08-18 16:48:25 +0300

    Example012

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -40,15 +40,28 @@ Console.WriteLine(year);
 
 // Вид 4 что то принимают и что то возвращают  
 
+// string Method4(int count, string text)
+// {
+//   int i = 0;
+//   string result = String.Empty;
+
+//     while (i < count)
+//     {
+//       result = result + text;
+//       i++;
+//     }
+//     return result;
+// }
+
 string Method4(int count, string text)
 {
-  int i = 0;
-  string result = String.Empty;
 
-    while (i < count)
+    string result = String.Empty;
+    for(int i = 0; i< count ; i++)
     {
+      
       result = result + text;
-      i++;
+    
     }
     return result;
 }
