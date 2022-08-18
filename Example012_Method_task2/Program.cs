dfec226b5c31dccc9e1ab68d620e2543de729f20
Alexei <vertuginalexei7@gmail.com>
2022-08-18 17:58:42 +0300

    Example012task2

---
+++ Example012_Method_task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Упорядочить массив 
+int [] arr ={1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int [] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}" );
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[]array)
+{
+    for(int i= 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j; // просто меняем знак > на < и наоборот получаем как от мин до макс так и от макс до мин.
+        }
+        
+        int temporary = array[i];
+        array[i]= array[minPosition];
+        array[minPosition]= temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
