80a95903a0b83f8c3ad0ccff05ec26d6d2927304
Alexei <vertuginalexei7@gmail.com>
2022-08-24 12:32:56 +0300

    examples013_2

---
+++ Example013_RecursionAlgorithm_num01/Program.cs
@@ -0,0 +1,44 @@
+﻿/// Пример с цифрами.
+// int [,] matrix = new int [3,4];
+
+// for (int i = 0; i < 3; i++ ) // по строкам либо можно использовать для поиска "matrix.GetLength(0)"
+// {
+//     for (int j = 0 ; j < 4; j ++) // внутренний по столбцам либо можно использовать для поиска "matrix.GetLength(1)"
+//     {
+//         Console.Write ($"{matrix[i, j]} ");
+//     }
+// Console.WriteLine();
+// }
+
+
+void PrintArray(int[,]matr)
+{
+   for (int i = 0; i < 3; i++ ) // по строкам либо можно использовать для поиска "matrix.GetLength(0)"
+   {
+    for (int j = 0 ; j < 4; j ++) // внутренний по столбцам либо можно использовать для поиска "matrix.GetLength(1)"
+    {
+          Console.Write ($"{matr[i, j]} ");
+    }
+   Console.WriteLine();
+   }
+}
+
+void FillArray(int[,]matr)// заполняем нашу матрицу случайными цифрами
+{
+   for (int i = 0; i < 3; i++ ) // по строкам либо можно использовать для поиска "matrix.GetLength(0)"
+   {
+    for (int j = 0 ; j < 4; j ++) // внутренний по столбцам либо можно использовать для поиска "matrix.GetLength(1)"
+    {
+        matr[i,j]= new Random().Next (1, 10);//[1, 10]
+    }
+   
+   }
+}
+
+int [,] matrix = new int [3,4];
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();// пробел пустая строка
+PrintArray(matrix);
+
