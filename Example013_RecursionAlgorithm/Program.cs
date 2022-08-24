315b25bcfb60c0fdf2cf4cdd4efff6ed0aa43bf4
Alexei <vertuginalexei7@gmail.com>
2022-08-24 11:47:18 +0300

    examples013

---
+++ Example013_RecursionAlgorithm/Program.cs
@@ -0,0 +1,14 @@
+﻿string[,] table = new string [2,5];
+// String.Empty - константа инициализация строк 
+// table [0,0] table [0,1] table [0,2] table [0,3] table [0,4] - столбики (индекс с 0 )
+// table [1,0] table [1,1] table [1,2] table [1,3] table [1,4] - меняем индекс строки
+
+table[1,2]="слово";
+
+for (int rows = 0; rows <2; rows++)
+{
+    for (int colums = 0; colums < 5;colums++)
+    {
+        Console.WriteLine($"-{table[rows,colums]}-");
+    }
+}
