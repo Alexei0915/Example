deb91a9f7c3912f02fb8664c701a81983b560a42
Alexei <vertuginalexei7@gmail.com>
2022-08-24 11:48:17 +0300

    examples013_1

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -1,4 +1,5 @@
-﻿string[,] table = new string [2,5];
+﻿// Пример со строками и столбцами 
+string[,] table = new string [2,5];
 // String.Empty - константа инициализация строк 
 // table [0,0] table [0,1] table [0,2] table [0,3] table [0,4] - столбики (индекс с 0 )
 // table [1,0] table [1,1] table [1,2] table [1,3] table [1,4] - меняем индекс строки
